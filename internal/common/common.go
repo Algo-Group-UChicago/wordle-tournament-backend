16f4d94a254953b039cb19332834ff2820a7e944
akpiya <akashpiya17@gmail.com>
2025-11-09 14:56:25 -0600

    move NumTargetWords to common mod

--- internal/common/common.go
+++ internal/common/common.go
@@ -3,3 +3,4 @@ package common
 // WordLength is the number of letters in a valid Wordle word
 const WordLength = 5
 const DummyGuess = "imagine guessing more than 5 letters"
+const NumTargetWords = 1000
