077838e83a4ac9092bf4050dd5e4caa03d037249
Akash Piya <48157425+akpiya@users.noreply.github.com>
2025-12-16 13:50:34 -0600

    Add Local Dynamo Instance Provisioning (#6)
    * add local dynamodb instance provisioning
    
    * remove unnecessary region + update readme

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,3 +1,24 @@
 package config
 
-const Port = "8080"
+import "os"
+
+type Config struct {
+	Port             string
+	Region           string
+	DynamoDBEndpoint string
+}
+
+func Load() Config {
+	return Config{
+		Port:             getEnv("PORT", "8080"),
+		Region:           getEnv("AWS_REGION", "us-east-1"),
+		DynamoDBEndpoint: getEnv("DYNAMODB_ENDPOINT", ""),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return defaultValue
+}
