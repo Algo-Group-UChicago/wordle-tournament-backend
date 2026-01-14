1356bdc05c2be31f77e66f4f2718e7166627977c
akpiya <akashpiya17@gmail.com>
2026-01-14 04:34:04 -0600

    edit common.go comment

--- internal/common/common.go
+++ internal/common/common.go
@@ -1,6 +1,6 @@
 package common
 
-// WordLength is the number of letters in a valid Wordle word
+// Constants that EXACTLY mirror library's constants
 const WordLength = 5
 const DummyGuess = "imagine guessing more than 5 letters"
 const NumTargetWords = 2315
